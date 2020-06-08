8c3358dfb9243b2bb3ec8c49c70f274fd42b3de4
c-cait <caitlinciaramella@gmail.com>
2020-06-07 21:26:34 -0700

    basic bones of components

---
+++ src/Components/Auth/Auth.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Auth extends Component {
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+            <div>
+                Auth
+            </div>
+        )
+    }
+}
+
+export default Auth;
