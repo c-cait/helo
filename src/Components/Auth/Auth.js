51a86b5188549f23b699b75726508aab65580a49
c-cait <caitlinciaramella@gmail.com>
2020-06-08 01:56:45 -0700

    styled auth page

--- src/Components/Auth/Auth.js
+++ src/Components/Auth/Auth.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import './Auth.css';
+import {FaRegGrinTongueWink} from 'react-icons/fa';
 
 class Auth extends Component {
     constructor(){
@@ -7,8 +9,22 @@ class Auth extends Component {
 
     render(){
         return(
-            <div>
-                Auth
+            <div className='auth-container'>
+                <div className='auth-section'>
+                    <FaRegGrinTongueWink className='wink'/>
+                    <div className='auth-title'> Helo </div>
+
+                    <form>
+                        <div className='input'>
+                            Username: <input className='input-box'></input>
+                        </div> 
+                        <div className='input'>
+                            Password: <input className='input-box'></input>
+                        </div>
+                        <button className='input-btn right'>Login</button>
+                        <button className='input-btn left'>Register</button>
+                    </form>
+                </div>
             </div>
         )
     }
