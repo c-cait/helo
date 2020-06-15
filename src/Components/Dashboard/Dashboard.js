025d55f9a97de2a2b5a75df99a537a83e47bd9ac
c-cait <caitlinciaramella@gmail.com>
2020-06-14 18:44:59 -0700

    styled search bar

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import './Dashboard.css'
+import './Dashboard.css';
+import {FiSearch} from 'react-icons/fi';
 
 class Dashboard extends Component {
     constructor(){
@@ -13,17 +14,27 @@ class Dashboard extends Component {
         return(
             <div className='dashboard-container'>
                 <div className='input-container'>
-                    <input
-                    type='text'
-                    placeholder='Search By Title'
-                    ></input>
-                    <button className='search-btn'>
-
-                    </button>
-                    <button className='reset-btn'>
-                        Reset
-                    </button>
+                    <div className='search-section'>
+                        <input
+                        className='input-box-dashboard'
+                        type='text'
+                        placeholder='Search By Title'
+                        ></input>
+                        <button className='search-btn'>
+                            <FiSearch className='search-icon'/>
+                        </button>
+                        <button className='reset-btn'>
+                            Reset
+                        </button>
+                    </div>
+                    <div className='my-posts'>
+                        My Posts
+                        <input 
+                        type='checkbox'>
+                        </input>
+                    </div>
                 </div>
+                
             </div>
         )
     }
